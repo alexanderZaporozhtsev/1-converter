f3c28584c055c29e0e449bc74d58acfaaeabd064
alexanderZaporozhtsev <randallyourmaker@gmail.com>
2025-07-28 13:46:05 +0000

    create function to get user input and empty convert function

--- main.go
+++ main.go
@@ -6,9 +6,18 @@ func main() {
 	const USDToEuro float64 = 0.8525
 	const USDToRub float64 = 79.40
 
-	euros := 150.0
+	amount := getUserInput()
 
-	eurosToRubles := euros / USDToEuro * USDToRub
+	eurosToRubles := amount / USDToEuro * USDToRub
 
 	fmt.Print(eurosToRubles)
 }
+
+func getUserInput() float64 {
+	var amount float64
+	fmt.Scan(&amount)
+
+	return amount
+}
+
+func convert(amount float64, convertFrom string, convertTo string) {}
